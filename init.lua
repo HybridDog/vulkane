2708ced335350252fef17d1b209ac5165fc9ad6e
Hybrid Dog <ovvv@web.de>
2017-12-30 18:02:54 +0100

    Remove obsolete update_map

--- init.lua
+++ init.lua
@@ -354,11 +354,6 @@ local function spawn_volcano(pos, h)
 	manip:set_data(data)
 	manip:write_to_map()
 
-	minetest.delay_function(16384, function(manip)
-		manip:update_map()
-		log"map updated"
-	end, manip)
-
 	--[[ just a few nodes in the middle of the tower are affected only when spawning a mountain while being inside sth
 	if occupied ~= 0 then
 		log(occupied.." node(s) were not set because there's already hard")
